73237f80edf357edcbaf6bc9bf7ec4e2006f4b18
unknown <derrickpereira1998@gmail.com>
2023-06-27 02:43:19 -0300

    fixed product detail loading big

--- src/Components/ProductDetails/index.tsx
+++ src/Components/ProductDetails/index.tsx
@@ -11,30 +11,32 @@ export default function ProductDetails() {
   const [product, setProduct] = useState<IProduct>()
 
   useEffect(() => {
-    if(product_id){
-      http.post('getProductData', {product_id})
-      .then(res => setProduct(res.data.data))
-      .catch(err => console.log(err))
+    if (product_id) {
+      http.post('getProductData', { product_id })
+        .then(res => setProduct(res.data.data))
+        .catch(err => console.log(err))
     }
   }, [product_id])
 
   return (
     <>
       <section className={styles.flex}>
-        <div className={styles.content}>
-          <img className={styles.content__img} src={product?.imagem} alt='Imagem do Produto' />
-          <div className={styles.content__details}>
-            <div className={styles.content__details__text}>
-              <h1 className={styles.content__details__text__title}>{product?.titulo}</h1>
-              <p className={styles.content__details__text__price}>R$ {product?.preco}</p>
-              <p className={styles.content__details__text__desc}>{product?.descricao}</p>
+        {product === undefined
+          ? <div className={styles.loading}></div>
+          : <div className={styles.content}>
+            <img className={styles.content__img} src={product?.imagem} alt='Imagem do Produto' />
+            <div className={styles.content__details}>
+              <div className={styles.content__details__text}>
+                <h1 className={styles.content__details__text__title}>{product?.titulo}</h1>
+                <p className={styles.content__details__text__price}>R$ {product?.preco}</p>
+                <p className={styles.content__details__text__desc}>{product?.descricao}</p>
+              </div>
+              <div className={styles.content__details__buttons}>
+                <button className={styles.content__details__buttons__button}>Comprar Agora</button>
+                <button className={styles.content__details__buttons__button}>Colocar no carrinho</button>
+              </div>
             </div>
-            <div className={styles.content__details__buttons}>
-              <button className={styles.content__details__buttons__button}>Comprar Agora</button>
-              <button className={styles.content__details__buttons__button}>Colocar no carrinho</button>
-            </div>
-          </div>
-        </div>
+          </div>}
       </section>
     </>
   )
